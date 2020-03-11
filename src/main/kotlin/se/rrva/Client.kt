efa8bf6603e67a9a304e306e4281c80f27bdbae7
Ragnar Rova <ragnar.rova@svt.se>
2020-03-11 22:13:34 +0100

    Genesis

---
+++ src/main/kotlin/se/rrva/Client.kt
@@ -0,0 +1,93 @@
+package se.rrva
+
+import com.sun.net.httpserver.HttpServer
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.client.request.request
+import io.ktor.client.request.url
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.slf4j.MDCContext
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import java.io.PrintWriter
+import java.net.InetSocketAddress
+import java.util.concurrent.CompletionStage
+
+class Client(val fooServiceUrl: String) {
+
+    val client = HttpClient(CIO) {
+        engine {
+            requestTimeout = 500
+            endpoint.connectTimeout = 100
+            endpoint.connectRetryAttempts = 2
+        }
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(
+                Json(
+                    JsonConfiguration(
+                        isLenient = true,
+                        ignoreUnknownKeys = true,
+                        serializeSpecialFloatingPointValues = true,
+                        useArrayPolymorphism = true
+                    )
+                )
+            )
+        }
+    }
+
+    suspend fun fetchFoo(ids: List<String>): List<Foo> {
+            return client.request {
+                header("User-Agent", "Foo")
+                parameter("input", ids.joinToString(","))
+                url("$fooServiceUrl/foo")
+            }
+    }
+
+}
+
+@Serializable
+data class Foo(val id: String)
+val client = Client("http://localhost:9090")
+
+fun fetchFooAsync(ids:List<String>): CompletionStage<List<Foo>> {
+    return CoroutineScope(Job() + Dispatchers.IO + MDCContext()).async {
+        try {
+            client.fetchFoo(ids)
+        } catch (e: Throwable) {
+            println(e.message)
+            emptyList<Foo>()
+        }
+    }.asCompletableFuture()
+}
+
+fun main() {
+    HttpServer.create(InetSocketAddress(9090), 1000).apply {
+
+        createContext("/foo") { http ->
+            http.responseHeaders.add("Content-type", "application/json")
+            http.sendResponseHeaders(200, 0)
+            PrintWriter(http.responseBody).use { out ->
+                out.println("""[{"id":"1"}]""")
+            }
+        }
+
+        start()
+    }
+    println("Hej")
+    val requests = (1..100).map {
+        fetchFooAsync(listOf(it.toString()))
+    }
+    for (request in requests) {
+        
+    }
+    fetchFooAsync(listOf(1001.toString())).thenAccept { println("got $it") }
+}
